a2ba614671427239e5213fa6f83210c295356da6
Varun Agrawal <varagrawal@gmail.com>
2014-07-05 09:00:02 +0530

    Version 2
    Updated to store birthdays locally in DB and improve retrieval
    efficiency.

---
+++ BirthdayBumper-WP8/BirthdayBumper/Models/BirthdayBumperContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using Microsoft.Phone.Data.Linq;
+using Microsoft.Phone.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayBumper.Models
+{
+    class BirthdayBumperContext : DataContext
+    {
+        // Specify the connection string as a static, used in main page and app.xaml.
+        public static string DBConnectionString = "Data Source=isostore:/BirthdayBumper.sdf";
+
+        // Pass the connection string to the base class.
+        public BirthdayBumperContext(string connectionString): base(connectionString) 
+        {
+            this.Friends = this.GetTable<Friend>();
+        }
+
+        // Specify a single table for the to-do items.
+        public Table<Friend> Friends { get; set; }
+    }
+
+}
